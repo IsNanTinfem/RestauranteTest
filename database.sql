620b856ccf5d5906059c0f0dd3703999192f0832
IsNaN <renan.oliveiradasilva.tinfem2022@gmail.com>
2024-11-29 14:52:09 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE restaurante;
+CREATE DATABASE IF NOT EXISTS restaurante;
 
 USE restaurante;
 
-CREATE TABLE estoque (
+CREATE TABLE IF NOT EXISTS estoque (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    quantidade INT NOT NULL
+    quantidade INT NOT NULL CHECK (quantidade >= 0)
 );
